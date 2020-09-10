233be25030e2146f7e42cd3f92848a1b7d938d9b
Vincent Ivos <vincentivos1@gmail.com>
2020-09-10 17:01:58 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,7 @@
 count = 10 
 while count >= 1 do
- puts count
- count _= 1
- puts "Happy New Year"
-end # Write your code here
+  puts count
+  count _= 1
+end  
+puts "Happy New Year"
+ # Write your code here
